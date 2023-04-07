f2cb2c79ce3d4bf71a0264c3ee403e3623e9193a
이충호 (주)소프트웍스/모바일 T월드 개발 <deed1515@softworks.co.kr>
2023-04-07 14:55:35 +0900

    [learn] vue 3 Composition API

--- vue3-book/src/App.vue
+++ vue3-book/src/App.vue
@@ -3,16 +3,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Reactivity />
+  <Watch />
 </template>
+
+<script>
+import Reactivity from '@views/Reactivity.vue';
+import Watch from '@views/Watch.vue';
+components: [Reactivity,Watch];
+
+</script>
 
 <style scoped>
 .logo {
---
+++ vue3-book/src/views/Watch.vue
@@ -0,0 +1,58 @@
+<template>
+    <p>{{ count_c_1 }}</p>
+    <button @click="count_c_1++">count_c_1 증가</button>
+    <p>{{ count_c_2 }}</p>
+    <button @click="count_c_2++">count_c_2 증가</button>
+    <p>{{ state }}</p>
+    <button @click="onStop()">watch Effect stop</button>
+</template>
+
+<script>
+import { ref, watch, watchEffect } from 'vue'
+export default {
+    setup () {
+        const count_c_1 = ref(0)
+        const count_c_2 = ref(0)
+        const state = ref('실행 중')
+
+        watch(
+            count_c_1,
+            (cur, prev) =>{
+                console.log(`Composition API watch : ${prev} => ${cur}`)
+            },
+            {
+                immediate: true,
+            }
+        )
+
+        watch([count_c_1, count_c_2], (cur, prev) => {
+            console.log(`Composition API multiple watch : ${prev} => ${cur}`)
+        })
+
+        const stop = watchEffect(
+            () => {
+                console.log(`Composition API watchEffect Call ${count_c_2.value}`)
+            },
+            {
+              flush: 'post',
+            }
+        )
+
+        const onStop = () => {
+          stop.value = '중지'
+          stop()
+        }
+
+        return {
+            count_c_1,
+            count_c_2,
+            state,
+            onStop
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
