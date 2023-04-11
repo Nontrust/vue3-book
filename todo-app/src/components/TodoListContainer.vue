f88ad8abcf74744517855ac7bce25115ce682713
이충호 (주)소프트웍스/모바일 T월드 개발 <deed1515@softworks.co.kr>
2023-04-11 16:40:02 +0900

    [init] todo-app

---
+++ todo-app/src/components/TodoListContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  
+</template>
+
+<script>
+import {ref, readonly, provide} from 'vue'
+export default {
+    name : 'TodoListContainer',
+    setup() {
+        const todos = ref([])
+        
+        const initTodos = (init_todos) =>{
+            todos.value = init_todos
+        }
+
+        const addTodo = (todo, date) => {}
+        const removeTodo = (id) => {}
+        const completeTodo = (id) => {}
+
+        provide('todos', readonly(todos))
+        provide('addTodo', readonly(addTodo))
+        provide('removeTodo', readonly(removeTodo))
+        provide('completeTodo', readonly(completeTodo))
+    }
+
+    // async created(){
+    
+    // }
+
+    
+}
+</script>
+
+<style>
+
+</style>
